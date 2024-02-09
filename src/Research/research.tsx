cc4e71365be8b522ba3b05d874813dc15811dd9d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 12:15:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/research.tsx
+++ src/Research/research.tsx
@@ -7,15 +7,23 @@ const research = () => {
         </div>
         <div className="md:grid grid-cols-3 gap-8">
             <div className="">
-                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="rounded h-full"/>
+                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="rounded h-56 w-full"/>
             </div>
             <div className="">
-                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-full" />
+                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-56 w-full" />
             </div>
             <div className="">
-                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="rounded h-full"/>
+                <img src="https://wallpapercave.com/wp/wp13396133.png" alt="" className="rounded h-56 w-full"/>
+            </div>    
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-56 w-full"/>
+            </div>        
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp13396133.png" alt="" className="rounded h-56 w-full"/>
             </div>
-            
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp13396133.png" alt="" className="rounded h-56 w-full"/>
+            </div>
         </div>
     </div>
   )
