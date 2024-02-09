f9e43e518ad4889327a09e436867cfc84a9c7f56
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 15:47:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/bioSci.tsx
+++ src/Research/bioSci.tsx
@@ -3,7 +3,7 @@ const bioSci = () => {
     <div>
         <div className="md:grid grid-cols-2">
             <div className="bg-gray-900 w-full text-white">
-                <p className="text-3xl text-yellow-500 pt-40 pl-16 pb-40">Biological Science</p>
+                <p className="text-3xl text-yellow-500 pt-40 pl-[10%] pb-40">Biological Science</p>
             </div>
             <div className="invisible md:visible">
                 <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="h-auto w-full"/>            
