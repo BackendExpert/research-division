a6e85ae4184cb989fa1b2532f724f67bc2a01936
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 12:09:52 +0530

    Udpate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/research.tsx
+++ src/Research/research.tsx
@@ -7,13 +7,13 @@ const research = () => {
         </div>
         <div className="md:grid grid-cols-3 gap-8">
             <div className="">
-                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="h-full w-auto" />
+                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="rounded h-full"/>
             </div>
             <div className="">
-                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="rounded" />
+                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-full" />
             </div>
             <div className="">
-                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" />
+                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="rounded h-full"/>
             </div>
             
         </div>
