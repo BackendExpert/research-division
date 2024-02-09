92c9b47ebc83e09d5e16fd22b8d55fbf3943d8f8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 12:04:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/research.tsx
+++ src/Research/research.tsx
@@ -1,7 +1,23 @@
 
 const research = () => {
   return (
-    <div>research</div>
+    <div className="container mx-auto px-10">
+        <div className="">
+            <p className="">Research Division at NIFS</p>
+        </div>
+        <div className="md:grid grid-cols-3 gap-8">
+            <div className="">
+                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="w-auto" />
+            </div>
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="w-96" />
+            </div>
+            <div className="">
+                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" />
+            </div>
+            
+        </div>
+    </div>
   )
 }
 
