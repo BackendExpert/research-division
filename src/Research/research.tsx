2c2c691899253270930a6230c4aa8e7e7a57fb44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 14:59:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/research.tsx
+++ src/Research/research.tsx
@@ -9,8 +9,8 @@ const research = () => {
             <p className="">Research Division at NIFS</p>
         </div>
         <div className="md:grid grid-cols-3 gap-8">
-            <div className="pt-[-20px]">
-                <Link to={'/Research/bioSci'}>
+            <div className="pt-[-20px] mb-20">
+                <Link to={'/Research/BioScience'}>
                     <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="rounded h-56 w-full object-cover"/>
                     <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
                     <div className="flex text-yellow-300 ml-8">
@@ -19,14 +19,25 @@ const research = () => {
                     </div>
                 </Link>
             </div>
-            <div className="">
+            <div className="mb-20">
                 <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-56 w-full" />
+                <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
+                    <div className="flex text-yellow-300 ml-8">
+                    <p className="">Explore more</p>
+                    <p className="pt-[3px]"><IonIcon name="chevron-forward-outline" /></p>
+                </div>
             </div>
-            <div className="">
+            <div className="mb-20">
                 <img src="https://wallpapercave.com/wp/wp13396133.png" alt="" className="rounded h-56 w-full"/>
+                <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
+                    <div className="flex text-yellow-300 ml-8">
+                    <p className="">Explore more</p>
+                    <p className="pt-[3px]"><IonIcon name="chevron-forward-outline" /></p>
+                </div>            
             </div>    
             <div className="">
                 <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-56 w-full"/>
+            
             </div>        
             <div className="">
                 <img src="https://wallpapercave.com/wp/wp12615291.jpg" alt="" className="rounded h-56 w-full"/>
