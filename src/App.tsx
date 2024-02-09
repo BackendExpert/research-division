69fb97caebd6e56d849a6b47cf39b1ba3a1f8292
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 14:48:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
           <Route path="/Research" element={<Research />}></Route>
+          <Route path="/Research/view" ></Route>
       </Routes>
     </BrowserRouter>
   )
