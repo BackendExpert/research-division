3f0f9275742929e0789da17bd1848de66b3bc44f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 15:07:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/research.tsx
+++ src/Research/research.tsx
@@ -30,20 +30,24 @@ const research = () => {
                 </Link>
             </div>
             <div className="mb-20">
-                <img src="https://wallpapercave.com/wp/wp13396133.png" alt="" className="rounded h-56 w-full"/>
-                <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
-                    <div className="flex text-yellow-300 ml-8">
-                    <p className="">Explore more</p>
-                    <p className="pt-[3px]"><IonIcon name="chevron-forward-outline" /></p>
-                </div>            
+                <Link to={'/Research/BioScience'}>
+                    <img src="https://wallpapercave.com/wp/wp13396133.png" alt="" className="rounded h-56 w-full"/>
+                    <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
+                        <div className="flex text-yellow-300 ml-8">
+                        <p className="">Explore more</p>
+                        <p className="pt-[3px]"><IonIcon name="chevron-forward-outline" /></p>
+                    </div>   
+                </Link>         
             </div>    
             <div className="mb-20">
-                <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-56 w-full"/>
-                <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
-                    <div className="flex text-yellow-300 ml-8">
-                    <p className="">Explore more</p>
-                    <p className="pt-[3px]"><IonIcon name="chevron-forward-outline" /></p>
-                </div>   
+                <Link to={'/Research/BioScience'}>
+                    <img src="https://wallpapercave.com/wp/wp2293434.jpg" alt="" className="rounded h-56 w-full"/>
+                    <p className="text-xl font-semibold mt-[-120px] ml-8 text-white">Biological Science</p>
+                        <div className="flex text-yellow-300 ml-8">
+                        <p className="">Explore more</p>
+                        <p className="pt-[3px]"><IonIcon name="chevron-forward-outline" /></p>
+                    </div> 
+                </Link>  
             </div>        
             <div className="mb-20">
                 <img src="https://wallpapercave.com/wp/wp12615291.jpg" alt="" className="rounded h-56 w-full"/>
