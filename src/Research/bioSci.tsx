064c91264d3da6d8505176e5e6a701b4a691a037
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 15:46:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Research/bioSci.tsx
+++ src/Research/bioSci.tsx
@@ -2,13 +2,13 @@ const bioSci = () => {
   return (
     <div>
         <div className="md:grid grid-cols-2">
-            <div className="">
-                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="h-auto w-full"/>
-                <p className="text-3xl text-white mt-[-220px] pl-16 pb-0 font-bold">Biological Science</p>
-            </div>
-            <div className="bg-gray-900 w-full text-white invisible md:visible">
+            <div className="bg-gray-900 w-full text-white">
                 <p className="text-3xl text-yellow-500 pt-40 pl-16 pb-40">Biological Science</p>
             </div>
+            <div className="invisible md:visible">
+                <img src="https://c1.wallpaperflare.com/preview/690/437/145/microscope-research-lab-laboratory.jpg" alt="" className="h-auto w-full"/>            
+            </div>
+
         </div>
         <div className="container mx-auto px-10">
             <div className="">
